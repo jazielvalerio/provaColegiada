d1dadaa01484aad5d7c5c39a1cd80d5bb0d359c6
jazielvalerio <jazielvalerio@gmail.com>
2019-01-23 20:52:41 -0200

    primeiro projeto

---
+++ colegiada-master/ProvaColegiada/src/main/java/br/ifpe/pg/provacolegiada/Home.java
@@ -0,0 +1,14 @@
+package br.ifpe.pg.provacolegiada;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+	
+	@GetMapping({"/","/home"})
+	public String exibirHome() {
+		return "home";
+	}
+	
+}
